3e11373747996609a2c3760dc04637b3206f430a
Berkay Tuncel <berkaytncl98@gmail.com>
2023-01-13 17:04:28 +0300

    Add chats ui

--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    @IBOutlet weak var imageView4: UIImageView!
+    @IBOutlet weak var imageView5: UIImageView!
+    @IBOutlet weak var imageView6: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        DispatchQueue.main.async {
+            self.imageView1.layer.cornerRadius = self.imageView1.bounds.size.width / 2.0
+            self.imageView1.clipsToBounds = true
+            
+            self.imageView2.layer.cornerRadius = self.imageView2.bounds.size.width / 2.0
+            self.imageView2.clipsToBounds = true
+            
+            self.imageView3.layer.cornerRadius = self.imageView3.bounds.size.width / 2.0
+            self.imageView3.clipsToBounds = true
+            
+            self.imageView4.layer.cornerRadius = self.imageView4.bounds.size.width / 2.0
+            self.imageView4.clipsToBounds = true
+            
+            self.imageView5.layer.cornerRadius = self.imageView5.bounds.size.width / 2.0
+            self.imageView5.clipsToBounds = true
+            
+            self.imageView6.layer.cornerRadius = self.imageView6.bounds.size.width / 2.0
+            self.imageView6.clipsToBounds = true
+        }
     }
-
-
 }
 
